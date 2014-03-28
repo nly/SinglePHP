b95bf902c7042a62904c630ec948aa26c1dd0490
niulingyun <nly92@foxmail.com>
2014-03-28 17:48:15 +0800

    fix大小写

--- app/tpl/doc.php
+++ app/tpl/doc.php
@@ -2,7 +2,7 @@
 $data = array(
     'title' => '详细文档',
 );
-View::tplInclude('Public/header', $data); ?>
+View::tplInclude('public/header', $data); ?>
    <p>原来的文档真的不能再看了~~~~~~还是看源代码吧</p>
 
-<?php View::tplInclude('Public/footer'); ?>
+<?php View::tplInclude('public/footer'); ?>
