f5d46a333739ae735ccda412528911c315fe7be2
leandre <nly92@foxmail.com>
2014-08-11 09:41:07 +0800

    关闭异常，所有异常交由程序处理、增加加载耗时选项

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,8 +1,10 @@
 <?php
 header("Content-Type:text/html; charset=utf-8");
-include '../SinglePHP.class.php';
+error_reporting(0);
+require '../SinglePHP.class.php';
 $config = array(
         'APP_PATH' => '../app/',
-        'USE_SESSION' => true
+        'USE_SESSION' => true,
+        'SHOW_LOAD_TIME' => true,
     );
 SinglePHP::getInstance($config) -> run();
